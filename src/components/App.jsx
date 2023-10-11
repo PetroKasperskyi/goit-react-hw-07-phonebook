42d3f1085566dbd99be080a6de59ef3fadb28d89
Petro Kasperskyi <124060604+PetroKasperskyi@users.noreply.github.com>
2023-10-11 09:12:50 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { GlobalStyle } from './GlobalStyle';
+import {
+  Wrapper,
+  Container,
+  TitlePhoneBook,
+  TitleContacts,
+} from './App.styled';
+
+
+
+export const App = () => (
+  <Wrapper>
+    <Container>
+      <TitlePhoneBook>Phonebook</TitlePhoneBook>
+      <ContactForm />
+    </Container>
+
+    <Container>
+      <TitleContacts>Contacts</TitleContacts>
+      <Filter />
+      <ContactList />
+      <GlobalStyle />
+    </Container>
+  </Wrapper>
+);
